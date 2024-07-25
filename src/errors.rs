5f74bc3827213f980b513f9ac0b6a3424eab7516
9oelM <hj923@hotmail.com>
2024-07-25 10:11:37 +0800

    feat: show link to the repo for all errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,7 @@
 use worker::{Method, Response};
 
+const GH_REF: &str = "See https://github.com/9oelM/embed-github for usage.";
+
 #[derive(thiserror::Error, Debug)]
 pub enum HighlightCodeError {
     #[error("Invalid highlight theme: {0}")]
@@ -10,13 +12,13 @@ pub enum HighlightCodeError {
 pub enum RequestError {
     #[error("Failed to construct url from {method:?} {path}")]
     ReqToUrl { method: Method, path: String },
-    #[error("Empty querystring. See https://github.com/9oelM/embed-github for usage.")]
+    #[error("Empty querystring")]
     EmptyQuerystring,
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum OptionsBuilderError {
-    #[error("Missing 'gh' query parameter. See https://github.com/9oelM/embed-github for usage.")]
+    #[error("Missing 'gh' query parameter")]
     MissingGhQuery,
     #[error(transparent)]
     RequestedSourceInfo(RequestedSourceInfoError),
@@ -46,15 +48,17 @@ pub enum DecodeLineRangeError {
 
 #[derive(thiserror::Error, Debug)]
 pub enum GetSourceError {
-    #[error("Provided line number {number} is out of range of the source code.")]
+    #[error("Provided line number {number} is out of range of the source code")]
     LineNumberOutOfRange { number: u64 },
     #[error(transparent)]
     FetchSource(worker::Error),
+    #[error("Failed to fetch source code from {url}. Status code: {status_code}")]
+    FetchSourceStatusCode { url: String, status_code: u16 },
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum ConvertGithubPermalinkToRawGithubUserContentError {
-    #[error("Provided URL {0} is too short. It's probably not a valid Github Permalink URL.")]
+    #[error("Provided URL {0} is too short. It's probably not a valid Github Permalink URL")]
     PathTooShort(String),
     #[error(
         "Could not construct correct URL pointing to raw.githubusercontent.com. Composed: {url}"
@@ -64,43 +68,42 @@ pub enum ConvertGithubPermalinkToRawGithubUserContentError {
 
 impl From<HighlightCodeError> for worker::Result<Response> {
     fn from(err: HighlightCodeError) -> Self {
-        Response::error(err.to_string(), 400)
+        Response::error(format!("{}. {}", err, GH_REF), 400)
     }
 }
 
 impl From<RequestError> for worker::Result<Response> {
     fn from(err: RequestError) -> Self {
-        // Status is always 400, so it's safe to unwrap
-        Response::error(err.to_string(), 400)
+        Response::error(format!("{}. {}", err, GH_REF), 400)
     }
 }
 
 impl From<OptionsBuilderError> for worker::Result<Response> {
     fn from(err: OptionsBuilderError) -> Self {
-        Response::error(err.to_string(), 400)
+        Response::error(format!("{}. {}", err, GH_REF), 400)
     }
 }
 
 impl From<RequestedSourceInfoError> for worker::Result<Response> {
     fn from(err: RequestedSourceInfoError) -> Self {
-        Response::error(err.to_string(), 400)
+        Response::error(format!("{}. {}", err, GH_REF), 400)
     }
 }
 
 impl From<DecodeLineRangeError> for worker::Result<Response> {
     fn from(err: DecodeLineRangeError) -> Self {
-        Response::error(err.to_string(), 400)
+        Response::error(format!("{}. {}", err, GH_REF), 400)
     }
 }
 
 impl From<GetSourceError> for worker::Result<Response> {
     fn from(err: GetSourceError) -> Self {
-        Response::error(err.to_string(), 400)
+        Response::error(format!("{}. {}", err, GH_REF), 400)
     }
 }
 
 impl From<ConvertGithubPermalinkToRawGithubUserContentError> for worker::Result<Response> {
     fn from(err: ConvertGithubPermalinkToRawGithubUserContentError) -> Self {
-        Response::error(err.to_string(), 400)
+        Response::error(format!("{}. {}", err, GH_REF), 400)
     }
 }
